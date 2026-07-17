53a9e4216c91021f15b04bf9a3747facb9eb635e
Vladimir <Bigteamer@yandex.ru>
2026-07-17 05:49:10 +0300

    limit

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useBodyParser('json', { limit: '12mb' });
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
